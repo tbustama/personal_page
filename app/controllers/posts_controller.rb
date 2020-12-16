980108db930da951e1314b58094ca9fa18322109
Taylor Bustamante <taylorbustamante16@gmail.com>
2020-12-16 07:44:35 -0800

    switched to postgres

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,6 @@ class PostsController < ApplicationController
 
     def show
         @post = Post.find(params[:id])
-        # byebug
     end
 
     def new
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_12_14_232053) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
